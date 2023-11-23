0b2e18cfd9fb1a92c14923c9df8e11219e532d38
Alex Dalene <aledal51469@stud.noroff.no>
2023-11-23 20:37:33 +0100

    Login e2e test update

--- cypress/integration/login.spec.js
+++ cypress/integration/login.spec.js
@@ -24,4 +24,24 @@ describe('User Login', () => {
 
     cy.url().should('include', '/profile');
   });
+
+  it('correctly handles error cases', () => {
+    cy.intercept('POST', 'https://api.noroff.dev/api/v1/auction/auth/login', {
+      statusCode: 400,
+    }).as('loginRequest');
+
+    cy.visit('/auth');
+
+    cy.get('#login-link').click();
+
+    cy.get('#form-login').within($form => {
+      cy.get('#email').type(userInfo.email);
+      cy.get('#password').type(userInfo.password);
+      cy.root().submit();
+    });
+
+    cy.wait('@loginRequest').its('response.statusCode').should('eq', 400);
+
+    cy.url().should('not.include', '/profile');
+  });
 });
