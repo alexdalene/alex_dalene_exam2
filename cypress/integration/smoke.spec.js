c369b802300bd22489b1dc3c116698b44acae114
Alex Dalene <aledal51469@stud.noroff.no>
2023-11-23 13:09:34 +0100

    Adds auth paths

--- cypress/integration/smoke.spec.js
+++ cypress/integration/smoke.spec.js
@@ -4,13 +4,18 @@ describe('Smoke Test', () => {
     cy.contains('Semester Project 2');
   });
 
+  it('can view the auth page', () => {
+    cy.visit('/auth');
+    cy.contains('Create a user');
+  });
+
   it('can view the profile page', () => {
-    cy.visit('/user/profile');
+    cy.visit('/auth/profile');
     cy.contains('Your Profile');
   });
 
   it('can view the create a listing page', () => {
-    cy.visit('/user/create-listing');
+    cy.visit('/auth/create-listing');
     cy.contains('Create a listing');
   });
 });
