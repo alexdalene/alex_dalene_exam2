efd6a749204fa07c1e4338eb7bd83dc9398db68d
Alex Dalene <aledal51469@stud.noroff.no>
2023-11-26 16:14:29 +0100

    Adds navbar html

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,37 @@ module.exports = {
     './auth/**/*.html',
   ],
   theme: {
-    extend: {},
+    colors: {
+      primary: '#BBC3FF',
+      secondary: '#46464F',
+      background: '#1B1B1F',
+      surface: '#212126',
+      surfaceVariant: '#2E2E38',
+      onSurfaceVariant: '#67626A',
+      text: '#E4E1E6',
+      error: '#FFB4AB',
+      accentGreen: '#D2F898',
+      accentPink: '#F7567C',
+    },
+    extend: {
+      fontSize: {
+        sm: '0.833rem',
+        base: '1rem',
+        xl: '1.200rem',
+        '2xl': '1.440rem',
+        '3xl': '1.728rem',
+        '4xl': '2.074rem',
+        '5xl': '2.489rem',
+      },
+      fontFamily: {
+        heading: ['Poppins', 'sans-serif'],
+        body: ['Gantari', 'sans-serif'],
+      },
+      fontWeight: {
+        normal: '400',
+        bold: '700',
+      },
+    },
   },
   plugins: [],
 };
