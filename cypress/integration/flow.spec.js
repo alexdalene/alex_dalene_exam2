afae0ebd517eada0b48fe255dc166576dd475df3
Alex Dalene <aledal51469@stud.noroff.no>
2023-11-30 14:34:05 +0100

    Trying to fix e2e tests being buggy

--- cypress/integration/flow.spec.js
+++ cypress/integration/flow.spec.js
@@ -2,6 +2,8 @@ describe('User Flow', () => {
   it('lets an unregistered user search through listings', () => {
     cy.visit('/browse');
 
+    cy.wait(1000);
+
     cy.get('#input-search').type('test');
 
     cy.get('#input-search').should('value', 'test');
