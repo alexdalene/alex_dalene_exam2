52104dc3ffa325ae8584c1db3a2d4a2ed8739326
Alex Dalene <aledal51469@stud.noroff.no>
2023-12-05 11:05:58 +0100

    Adds update avatar functionality

--- src/js/pages/profile.mjs
+++ src/js/pages/profile.mjs
@@ -2,6 +2,7 @@ import { navbar } from '../components/navbar/navbar.mjs';
 import { load } from '../storage/load.mjs';
 import { loader } from '../components/loader/loader.mjs';
 import { profile } from '../components/profile/profile.mjs';
+import { checkAvatar } from '../functions/profile/update.mjs';
 
 const render = async () => {
   try {
@@ -13,6 +14,7 @@ const render = async () => {
     loader.showLoader();
     await profile();
     await navbar();
+    checkAvatar();
     loader.hideLoader();
   } catch (error) {
     console.error(error);
@@ -20,18 +22,3 @@ const render = async () => {
 };
 
 render();
-
-const update = document.querySelector('#update-avatar');
-const button = document.querySelector('#update-button');
-
-button.addEventListener('click', () => {
-  if (button.textContent === 'check') {
-    if (update.value && typeof update.value === 'string') {
-      console.log(update.value);
-      update.value = '';
-    }
-    button.textContent = 'edit';
-  } else {
-    button.textContent = 'check';
-  }
-});
