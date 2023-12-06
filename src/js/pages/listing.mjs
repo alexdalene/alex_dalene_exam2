c9808d77fa29ec0e3d32b95572dd27f31e15512b
Alex Dalene <aledal51469@stud.noroff.no>
2023-12-06 15:45:13 +0100

    Adds try..catch to async function

--- src/js/pages/listing.mjs
+++ src/js/pages/listing.mjs
@@ -1,8 +1,13 @@
 import { getSpecificListing } from '../api/listing/listing.mjs';
+import { navbar } from '../components/navbar/navbar.mjs';
+import { loader } from '../components/loader/loader.mjs';
 
 const render = async () => {
   try {
+    loader.showLoader();
     await getSpecificListing();
+    await navbar();
+    loader.hideLoader();
   } catch (error) {
     console.log(error);
   }
