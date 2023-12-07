799b2925b2dbeb8701bc3f9b62c65a30054280da
Alex Dalene <aledal51469@stud.noroff.no>
2023-12-07 10:13:15 +0100

    Adds helper functions for singleListing

---
+++ src/js/components/listings/single-createContainer.mjs
@@ -0,0 +1,31 @@
+// Helper function to create bid container
+export const createBidContainer = (amount, icon, label) => {
+  const bidContainer = document.createElement('div');
+  bidContainer.classList.add(
+    'border',
+    'border-zinc-700',
+    'rounded-xl',
+    'h-20',
+    'p-4',
+    'flex',
+    'justify-center',
+    'flex-col',
+  );
+
+  const amountElement = document.createElement('p');
+  amountElement.classList.add('text-2xl', 'font-bold');
+  amountElement.textContent = amount;
+
+  const labelElement = document.createElement('h3');
+  labelElement.classList.add('flex', 'items-center', 'gap-2', 'text-sm');
+  const iconElement = document.createElement('span');
+  iconElement.classList.add('material-symbols-outlined', 'text-purple-300');
+  iconElement.textContent = icon;
+  labelElement.appendChild(iconElement);
+  labelElement.appendChild(document.createTextNode(label));
+
+  bidContainer.appendChild(amountElement);
+  bidContainer.appendChild(labelElement);
+
+  return bidContainer;
+};
