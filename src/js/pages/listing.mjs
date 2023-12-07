0d3946dbe6b12f588e20685f8b042556a33a5519
Alex Dalene <aledal51469@stud.noroff.no>
2023-12-07 10:14:12 +0100

    Adds function for appending single listing to page

--- src/js/pages/listing.mjs
+++ src/js/pages/listing.mjs
@@ -1,12 +1,12 @@
-import { getSpecificListing } from '../api/listing/listing.mjs';
 import { navbar } from '../components/navbar/navbar.mjs';
 import { loader } from '../components/loader/loader.mjs';
 import { checkStorage } from '../functions/listings/checkStorage.mjs';
+import { displaySingleListing } from '../views/listings/single.mjs';
 
 const render = async () => {
   try {
     loader.showLoader();
-    await getSpecificListing();
+    await displaySingleListing();
     await checkStorage();
     await navbar();
     loader.hideLoader();
