216c00c96224422261f4c534bc7157093520aecc
Alex Dalene <aledal51469@stud.noroff.no>
2023-12-11 09:47:22 +0100

    Updates fetch to include values

--- src/js/api/listing/create.mjs
+++ src/js/api/listing/create.mjs
@@ -1,7 +1,7 @@
 import { apiBase } from '../base.mjs';
 import { headers } from '../headers.mjs';
 
-export async function createListing() {
+export async function createListing(title, description, tags, media, endsAt) {
   const response = await fetch(`${apiBase}/auction/listings`, {
     method: 'post',
     body: JSON.stringify({ title, description, tags, media, endsAt }),
@@ -14,5 +14,6 @@ export async function createListing() {
     throw new Error(data.errors[0].message);
   }
 
+  console.log(data);
   return data;
 }
